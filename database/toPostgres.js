60d966b669666e91758b57eb588fef8abf46eec5
Cory Zauss <coryclassic1989@gmail.com>
2022-01-12 18:06:26 -0500

    used fs createReadStream to access chunks of csv data

--- database/toPostgres.js
+++ database/toPostgres.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
+const path = require('path');
+
+
 //scan thru && identify errors or correct errors/inconsistant data
 // run data thru schemas and store in database
 
-//
-const streamData = fs.createReadStream(__dirname, '../Data/products.csv', utf8);
+
+var chunks = 0;
+
+const streamData = fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8');
 
 streamData.on('data', function(chunk) {
-  console.log(chunk)
-
+  chunks++
+  console.log(chunks)
 })
+
