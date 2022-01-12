2499d7ed74458f6fed15c9a28210c95f40dfb8d0
Cory Zauss <coryclassic1989@gmail.com>
2022-01-12 18:20:11 -0500

    read one line at a time from csvs

--- database/toPostgres.js
+++ database/toPostgres.js
@@ -1,17 +1,16 @@
 const fs = require('fs')
-const path = require('path');
 
+const lineReader = require('readline')
 
 //scan thru && identify errors or correct errors/inconsistant data
 // run data thru schemas and store in database
-
-
-var chunks = 0;
-
-const streamData = fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8');
-
-streamData.on('data', function(chunk) {
-  chunks++
-  console.log(chunks)
+ var read = lineReader.createInterface({
+  input: fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8')
 })
+
+read.on('line', (line) => {
+  console.log(line)
+  // streams one complete line at a time from the csv files
+})
+
 
