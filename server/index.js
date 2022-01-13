5aad6a79adabe5b31c795d74a65976259f1b8a24
Cory Zauss <coryclassic1989@gmail.com>
2022-01-12 21:12:47 -0500

    update

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const PORT = 3000 ;
 const cors = require('cors');
 const axios = require('axios');
 
-app.use(express.json())
 
+
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 })
+
+module.exports = {
+  pool:
+}
