e4b7ca1a98b29d2897c7baecdf08a7e56273ce0f
Cory Zauss <coryclassic1989@gmail.com>
2022-01-12 23:47:53 -0500

    products csv etl

--- server/database/index.js
+++ server/database/index.js
@@ -3,22 +3,9 @@ const {Pool} = require('pg')
 const pool = new Pool({
   host: 'localhost',
   user: 'coryzauss',
-  database: 'password',
+  database: 'Products',
+  password: 'password',
   port: 5432
 })
 
-let connection = pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack)
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release()
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows)
-  })
-})
-
-
-module.exports = connection
+module.exports = pool
--- server/database/toPostgres.js
+++ server/database/toPostgres.js
@@ -1,50 +1,88 @@
 const fs = require('fs');
-const lineReader = require('readline');
+const readline = require('readline');
 const pool = require('./index.js')
-
-
 
 //scan thru && identify errors or correct errors/inconsistant data
 // run data thru schemas and store in database
- var read = lineReader.createInterface({
-  input: fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8')
-})
+//  var read = lineReader.createInterface({
+//   input: fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8')
+// })
 
-read.on('line', (line) => {
-  let split = line.split(',')
-  console.log(split)
-  // pool.query
+// read.on('line', async (line) => {
+//   let split = line.split(',')
+//   console.log(split)
+//    const client = await pool.connect()
+//    await client.query(`INSERT INTO products VALUES(?,?,?,?,?,?)`, [split[0], split[1], split[2], split[3], split[4], split[5]])
+//    await client.release();
+//    console.log('released')
+
+//   // streams one complete line at a time from the csv files
+// })
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('/Users/coryzauss/Projects/sdc/Data/products.csv', 'utf8');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+  var readHeader = false
+  var counter = 0;
+  var query = 'INSERT INTO products(id, name, slogan, description, category, default_price) VALUES ';
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as line.
+    if (!readHeader) {
+      readHeader= true;
+      continue;
+    }
+
+    if (counter >= 65000) {
+      query = query.slice( 0, query.length -1)
+     // console.log('QUERY', query)
+      try{
+        const client = await pool.connect()
+        await client.query(query)
+        await client.release();
+      } catch(err) {
+        console.log(' q failed', err, query)
+        throw new Error('failure')
+      }
+      query = 'INSERT INTO products(id, name, slogan, description, category, default_price) VALUES ';
+      counter = 0;
+    }
+    let split = line.split(',');
+    var id = split[0]
+    var name =  split[1].replace(/['"]+/g, '')
+    var slogan = split[2].replace(/['"]+/g, '')
+    var description = split[3].replace(/['"]+/g, '')
+    var category = split[4].replace(/['"]+/g, '')
+    var default_price = split[5].replace('"default_price": ', '')
 
 
-  // streams one complete line at a time from the csv files
-})
+
+    counter++;
+    var temp = `(${id}, '${name}', '${slogan}', '${description}', '${category}', ${default_price}),`
+    if (split.length <= 6) {
+      // if problem skip record
+      query += temp
+    } else {
+
+      console.log('PROBLEMO:', temp)
+    }
+
+    if (line.includes('"id"') ) {
+      console.log('line:::::',line)
+    }
 
 
-/*
-create: function ({body}) {
-    connection = db.connection();
-    return new Promise(function(resolve, reject) {
-      let sql = `INSERT INTO messages(user_name, text_value, room_name ) VALUES ("${body.username}", "${body.message}", "${body.roomname}" )`;
-      connection.query(sql, (err, result) => {
-        if (err) {
-          reject(err);
-          connection.end();
-        } else {
-          console.log('insert sucessful');
-          resolve(result);
-          connection.end();
-        }
-      });
-    });
   }
-};
+  fileStream.close()
+console.log('finish')
+}
+
+processLineByLine();
 
 
-id serial,
-  name VARCHAR,
-  slogan VARCHAR,
-  description VARCHAR,
-  category VARCHAR,
-  default_price INT,
-  PRIMARY KEY(id)
-*/
+
